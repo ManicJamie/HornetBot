55bf3d8f4a693e65f6b4e7c0a7674cc0f8d89577
ManicJamie <jamiebloomfield8@gmail.com>
2023-07-11 23:28:58 +0100

    Case insensitive module help

--- components/helpcmd.py
+++ components/helpcmd.py
@@ -6,6 +6,8 @@ import discord
 import discord.embeds as e
 from discord.ext.commands.core import Command
 
+from discord.ext.commands._types import BotT
+
 from components import embeds
 
 class HornetHelpCommand(HelpCommand):
@@ -48,6 +50,51 @@ class HornetHelpCommand(HelpCommand):
         helpmessage = command.description if command.description else (command.help if command.help else "")
         await embeds.embedMessage(self.get_destination(), title=f"{self.context.prefix}{command.qualified_name} {getParams(command)}", \
                                     message=aliases+helpmessage)
+    
+    async def command_callback(self, ctx: Context[BotT], /, *, command: Optional[str] = None) -> None:
+        """Override of default callback for case-insensitive cog help implementation"""
+        await self.prepare_help_command(ctx, command)
+
+        bot : commands.Bot = ctx.bot
+
+        if command is None:
+            mapping = self.get_bot_mapping()
+            return await self.send_bot_help(mapping)
+
+        # Check if it's a cog
+        cogs = bot.cogs
+        cogs = {k.lower():v for (k, v) in bot.cogs.items()}
+        if command.lower() in cogs.keys():
+            return await self.send_cog_help(cogs[command.lower()])
+
+        maybe_coro = discord.utils.maybe_coroutine
+
+        # If it's not a cog then it's a command.
+        # Since we want to have detailed errors when someone
+        # passes an invalid subcommand, we need to walk through
+        # the command group chain ourselves.
+        keys = command.split(' ')
+        cmd = bot.all_commands.get(keys[0])
+        if cmd is None:
+            string = await maybe_coro(self.command_not_found, self.remove_mentions(keys[0]))
+            return await self.send_error_message(string)
+
+        for key in keys[1:]:
+            try:
+                found = cmd.all_commands.get(key)  # type: ignore
+            except AttributeError:
+                string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                return await self.send_error_message(string)
+            else:
+                if found is None:
+                    string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                    return await self.send_error_message(string)
+                cmd = found
+
+        if isinstance(cmd, commands.Group):
+            return await self.send_group_help(cmd)
+        else:
+            return await self.send_command_help(cmd)
 
 def getParams(cmd: Command):
     paramstring = ""
