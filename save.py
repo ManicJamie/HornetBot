f79e3c44b95d05932696f69df08e6744189e47f1
ManicJamie <jamiebloomfield8@gmail.com>
2023-08-21 02:05:53 +0100

    Global modules + Full template enforcement

--- save.py
+++ save.py
@@ -7,12 +7,15 @@ VERSION = 0.1
 FULL_TEMPLATE = {
     "version": VERSION,
     "module_templates": {},
+    "global_module_templates": {},
     "guild_template": {
         "nick" : "", # NB: automatically set to guild name on addition
         "adminRoles" : [],
         "spoileredPlayers": [],
+        "logChannel": None,
         "modules" : {} # NB: filled with module_templates on guild instantiation
     },
+    "modules": {}, # NB: filled with global_module_templates on module instantiation
     "guilds": {}
 }
 
@@ -25,21 +28,6 @@ def save():
 
     with open(JSON_PATH, "w") as f:
         json.dump(data, f, indent=4)
-
-if not os.path.exists(JSON_PATH):
-    data = FULL_TEMPLATE
-    save()
-else:
-    with open(JSON_PATH) as f:
-        try:
-            data = json.load(f)
-        except json.JSONDecodeError:
-            logging.warn("Could not deserialise save.json - loading backup")
-            with open(JSON_PATH + ".bak") as f2:
-                data = json.load(f2)
-        if VERSION > data["version"]:
-            logging.error(f"Save json is out of date! Json ver: {data['version']} < Save ver: {VERSION}")
-            exit(11)
 
 def add_module_template(module_name: str, init_data: dict):
     data["module_templates"][module_name] = copy.deepcopy(init_data)
@@ -63,6 +51,9 @@ def get_guild_ids() -> list[str]:
 def get_module_data(guild_id, module_name):
     return get_guild_data(guild_id)["modules"][module_name]
 
+def get_global_module(module_name):
+    return data["global_module_templates"][module_name]
+
 def get_guild_data(guild_id):
     if str(guild_id) not in data["guilds"]:
         logging.warn("Guild not found: instantiating")
@@ -77,7 +68,11 @@ def init_guild_data(guild_id : str, guild_name : str = ""):
 
 def init_module(module_name, init_data=None):
     if init_data is None:
-        if module_name not in data["module_templates"]: return # No template added by module, ignore it
+        if module_name not in data["module_templates"]: 
+            if module_name in data["global_module_templates"]:
+                init_global_module(module_name, init_data)
+            else:
+                return # No template added by module, ignore it
         init_data = data["module_templates"][module_name]
     for guild_id in get_guild_ids():
         modules = get_guild_data(guild_id)["modules"]
@@ -86,3 +81,32 @@ def init_module(module_name, init_data=None):
         else:
             modules[module_name] = copy.deepcopy(init_data)
     save()
+
+def init_global_module(module_name, init_data=None):
+    if init_data is None:
+        if module_name not in data["global_module_templates"]: return # No template added by module, ignore it
+        init_data = data["global_module_templates"][module_name]
+    if module_name in data["modules"]:
+        enforce_template_dict(data["modules"][module_name], init_data)
+    else:
+        data["modules"][module_name] = copy.deepcopy(init_data)
+    save()
+
+if not os.path.exists(JSON_PATH):
+    data = FULL_TEMPLATE
+    save()
+else:
+    with open(JSON_PATH) as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            logging.warn("Could not deserialise save.json - loading backup")
+            with open(JSON_PATH + ".bak") as f2:
+                data = json.load(f2)
+        if VERSION > data["version"]:
+            logging.error(f"Save json is out of date! Json ver: {data['version']} < Save ver: {VERSION}")
+            exit(11)
+    
+    enforce_template_dict(data, FULL_TEMPLATE)
+    save()
+    pass
